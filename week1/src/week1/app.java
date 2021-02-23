ba42cb919b4259da6e252acd918d77f6722c6405
Adam Brown <adam@Adams-MacBook-Air.local>
2021-02-22 21:55:58 -0700

    Add week1 folder

---
+++ week1/src/week1/app.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Assign a variable for the price of an item
+		
+		double itemPrice = 14.99;
+		System.out.println(itemPrice);
+
+	}
+
+}
